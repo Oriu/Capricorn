e78521a2ab9dbcbfd7993f259568de6f397f751c
Oriu <837934001@qq.com>
2020-01-04 17:01:16 +0800

    1、redis key 的基础通用方法 2、reids string 的基础通用方法

--- Capricorn.Infrastructure/Cache/Capricorn.Cache.Redis/RedisCache.cs
+++ Capricorn.Infrastructure/Cache/Capricorn.Cache.Redis/RedisCache.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Capricorn.Cache.Redis
 {
-    public class RedisCache
+    public partial class RedisCache
     {
         private static ConnectionMultiplexer redisConnection { get; }
         static RedisCache()
@@ -17,23 +17,83 @@ namespace Capricorn.Cache.Redis
         }
 
         /// <summary>
-        /// 添加缓存
+        /// 缓存中是否存在键
         /// </summary>
         /// <param name="key">键</param>
-        /// <param name="value">值</param>
-        public async Task SetStringAsync(string key, string value)
+        /// <param name="dbid">redis数据库id</param>
+        /// <returns></returns>
+        public async Task<bool> KeyExistsAsync(string key, int dbid)
         {
-            await redisConnection.GetDatabase().StringSetAsync(key, value, TimeSpan.FromSeconds(20));
+            return await redisConnection.GetDatabase(dbid).KeyExistsAsync(key);
         }
 
         /// <summary>
-        /// 获取缓存
+        /// 删除key
         /// </summary>
         /// <param name="key">键</param>
-        public async Task<string> GetStringAsync(string key)
+        /// <param name="dbid">redis数据库id</param>
+        /// <returns></returns>
+        public async Task<bool> KeyDeleteAsync(string key, int dbid)
         {
-            return await redisConnection.GetDatabase().StringGetAsync(key); 
+            return await redisConnection.GetDatabase(dbid).KeyDeleteAsync(key);
         }
 
+        /// <summary>
+        /// 设置key的过期时间
+        /// </summary>
+        /// <param name="key">键</param>
+        /// <param name="dbid">redis数据库id</param>
+        /// <param name="expiry">过期时间</param>
+        /// <returns></returns>
+        public async Task<bool> KeyExpireAsync(string key, int dbid, TimeSpan expiry)
+        {
+            return await redisConnection.GetDatabase(dbid).KeyExpireAsync(key, expiry);
+        }
+
+        /// <summary>
+        /// 移除key的过期时间
+        /// </summary>
+        /// <param name="key">键</param>
+        /// <param name="dbid">redis数据库id</param>
+        /// <returns></returns>
+        public async Task<bool> KeyPersistAsync(string key, int dbid)
+        {
+            return await redisConnection.GetDatabase(dbid).KeyPersistAsync(key);
+        }
+
+        /// <summary>
+        /// 修改key
+        /// </summary>
+        /// <param name="oldKey">旧键</param>
+        /// <param name="newKey">新键</param>
+        /// <param name="dbid">redis数据库id</param>
+        /// <returns></returns>
+        public async Task<bool> KeyRenameAsync(string oldKey, string newKey, int dbid)
+        {
+            return await redisConnection.GetDatabase(dbid).KeyRenameAsync(oldKey, newKey);
+        }
+
+        /// <summary>
+        /// 返回key中存贮的数据类型
+        /// </summary>
+        /// <param name="key">键</param>
+        /// <param name="dbid">redis数据库id</param>
+        /// <returns></returns>
+        public async Task<RedisType> KeyTypeAsync(string key, int dbid)
+        {
+            return await redisConnection.GetDatabase(dbid).KeyTypeAsync(key);
+        }
+
+        /// <summary>
+        /// 将key移至另一个redis数据库
+        /// </summary>
+        /// <param name="key">键</param>
+        /// <param name="olddbid">老redis数据库id</param>
+        /// <param name="newdbid">新redis数据库id</param>
+        /// <returns></returns>
+        public async Task<bool> KeyMoveAsync(string key, int olddbid, int newdbid)
+        {
+            return await redisConnection.GetDatabase(olddbid).KeyMoveAsync(key, newdbid);
+        }
     }
 }
---
+++ Capricorn.Infrastructure/Cache/Capricorn.Cache.Redis/RedisString.cs
@@ -0,0 +1,99 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capricorn.Cache.Redis
+{
+    public partial class RedisCache
+    {
+        /// <summary>
+        /// 设置指定key得值(key存在则替换,不存在则创建)
+        /// </summary>
+        /// <param name="key">键</param>
+        /// <param name="value">值</param>
+        /// <param name="dbid">redis数据库id</param>
+        /// <param name="expiry">过期时间</param>
+        public async Task<bool> SetStringAsync(string key, string value, int dbid, TimeSpan? expiry = null)
+        {
+            return await redisConnection.GetDatabase(dbid).StringSetAsync(key, value, expiry);
+        }
+
+        /// <summary>
+        /// 设置多个key-value进入缓存
+        /// </summary>
+        /// <param name="values">key-value</param>
+        /// <param name="dbid">redis数据库id</param>
+        /// <returns></returns>
+        public async Task<bool> SetStringsAsync(KeyValuePair<RedisKey, RedisValue>[] values, int dbid)
+        {         
+            return await redisConnection.GetDatabase().StringSetAsync(values);
+        }
+
+        /// <summary>
+        /// 获取指定key得值(key不存在返回null)
+        /// </summary>
+        /// <param name="key">键</param>
+        public async Task<string> GetStringAsync(string key, int dbid)
+        {
+            return await redisConnection.GetDatabase(dbid).StringGetAsync(key);
+        }
+
+        /// <summary>
+        /// 获取多个key值
+        /// </summary>
+        /// <param name="keys">键数组</param>
+        /// <param name="dbid">redis数据库id</param>
+        /// <returns></returns>
+        public async Task<string[]> StringGetsAsync(string[] keys, int dbid)
+        {
+            if (keys.Length == 0)
+                return new string[] { };
+            List<RedisKey> redisKeys = new List<RedisKey>();
+            foreach (var key in keys)
+            {
+                redisKeys.Add(key);
+            }
+            RedisValue[] redisValues = await redisConnection.GetDatabase(dbid).StringGetAsync(redisKeys.ToArray());
+            return redisValues.ToStringArray();
+        }
+
+        /// <summary>
+        /// 追加字符串缓存(键不存在，则新增)
+        /// </summary>
+        /// <param name="key">键</param>
+        /// <param name="value">值</param>
+        /// <param name="dbid">redis数据库id</param>
+        public async Task<bool> StringAppendAsync(string key, string value, int dbid)
+        {
+            var length = await redisConnection.GetDatabase(dbid).StringAppendAsync(key, value);
+            return length > 0;
+        }
+
+        /// <summary>
+        /// 获取指定key的值的字符串子串(键不存在，返回空字符串)
+        /// </summary>
+        /// <param name="key">键</param>
+        /// <param name="start">开始位置</param>
+        /// <param name="end">结束位置</param>
+        /// <param name="dbid">redis数据库id</param>
+        /// <returns></returns>
+        public async Task<string> StringGetRangeAsync(string key, int start, int end, int dbid)
+        {
+            return await redisConnection.GetDatabase(dbid).StringGetRangeAsync(key, start, end);
+        }
+
+        /// <summary>
+        /// 获取指定key存储的字符串长度
+        /// </summary>
+        /// <param name="key">键</param>
+        /// <param name="dbid">redis数据库id</param>
+        public async Task<long> StringLengthAsync(string key, int dbid)
+        {
+            return await redisConnection.GetDatabase(dbid).StringLengthAsync(key);
+        }
+
+
+    }
+}
